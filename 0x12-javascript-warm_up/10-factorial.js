4690750926c09fc5bf59ae994e3dded686dc82cc
BroCrown <bereketaklile@gmail.com>
2022-11-28 19:03:55 -0800

    0

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -9,15 +9,4 @@ function factorial (numb) {
   }
 }
 
-console.log(factorial(numb));#!/usr/bin/node
-const numb = process.argv[2];
-
-function factorial (numb) {
-  if (numb <= 1 || isNaN(numb)) {
-    return 1;
-  } else {
-    return numb * factorial(numb - 1);
-  }
-}
-
 console.log(factorial(numb));
